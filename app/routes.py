22722bbd704c3425b8dc7b0cf43288487e808581
dragon <isanoguchi721@gmail.com>
2022-11-08 23:02:17 -0800

    fix

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
-from controllers import find
+from controllers import dataController
 
 router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-router.include_router(find.router)
+router.include_router(dataController.router)
